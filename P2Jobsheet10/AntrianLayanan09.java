b58b235cc4133cf958493272c8641bb371be456d
KarW010 <kartikostudy@gmail.com>
2025-05-06 16:00:44 +0700

    Praktikum 2

--- P2Jobsheet10/AntrianLayanan09.java
+++ P2Jobsheet10/AntrianLayanan09.java
@@ -90,4 +90,14 @@ public class AntrianLayanan09 {
         return mhs;
     }
 
+    public void lihatAkhir() {
+        if (IsEmpty()) {
+            System.out.println("Antrian kosong.");
+        } else {
+            System.out.println("Mahasiswa paling belakang: ");
+            System.out.println("NIM - NAMA - PRODI - KELAS");
+            data[rear].tampilkanData();
+        }
+    }
+    
 }
